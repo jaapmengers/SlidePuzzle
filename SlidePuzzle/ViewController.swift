a60cfeedff27fb840dbc1423f903dd4c814d704b
Jaap Mengers <jaapm@q42.nl>
2015-12-14 20:50:56 +0100

    Initial Commit

---
+++ SlidePuzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidePuzzle
+//
+//  Created by Jaap Mengers on 14/12/15.
+//  Copyright © 2015 Jaap Mengers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
