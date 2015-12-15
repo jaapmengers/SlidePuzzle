b3f95c44dfa61e405a2f71d0845668d418c5975e
Jaap Mengers <jaapm@q42.nl>
2015-12-15 22:32:48 +0100

    Adds a preliminary gesture recognizer

--- SlidePuzzle/ViewController.swift
+++ SlidePuzzle/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var image4: UIImageView!
+    
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        images = [image1, image2, image3, image4]
+        
+        for image in images {
+            image.userInteractionEnabled = true
+            
+            let recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+            image.addGestureRecognizer(recognizer)
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        
+        print("Handle pan")
+        
+        let translation = recognizer.translationInView(self.view)
+        if let view = recognizer.view{
+            view.center = CGPoint(x: view.center.x, y: view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPointZero, inView: self.view)
     }
 
 
