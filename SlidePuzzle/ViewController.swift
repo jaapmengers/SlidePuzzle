14c2b5a2fc95578df5cb1ad3b3478fb13487a0b5
Jaap Mengers <jaapm@q42.nl>
2015-12-20 16:35:04 +0100

    Shuffles the pieces and places them on the grid

--- SlidePuzzle/ViewController.swift
+++ SlidePuzzle/ViewController.swift
@@ -8,6 +8,29 @@
 
 import UIKit
 
+extension CollectionType {
+    /// Return a copy of `self` with its elements shuffled
+    func shuffle() -> [Generator.Element] {
+        var list = Array(self)
+        list.shuffleInPlace()
+        return list
+    }
+}
+
+extension MutableCollectionType where Index == Int {
+    /// Shuffle the elements of `self` in-place.
+    mutating func shuffleInPlace() {
+        // empty and single-element collections don't shuffle
+        if count < 2 { return }
+        
+        for i in 0..<count - 1 {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
+        }
+    }
+}
+
 struct Position {
     let x: Int
     let y: Int
@@ -42,7 +65,9 @@ class ViewController: UIViewController {
         
         let positions = Array(0..<16).map { Position(x: $0 % 4, y: Int(floor(Double($0) / 4.0)) ) }
         
-        for (index, pos) in positions.enumerate() {
+        let shuffled = positions.shuffle()
+        
+        for (index, pos) in shuffled.enumerate() {
             print(index, pos)
             
             let destView = views[index]
